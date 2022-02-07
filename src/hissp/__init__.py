dc52a5eba1df86ee1b8a627094109f3ef60a9679
gilch <gilch@users.noreply.github.com>
2022-02-07 00:57:12 -0700

    Add Apache boilerplate to __init__.py

--- src/hissp/__init__.py
+++ src/hissp/__init__.py
@@ -1,5 +1,18 @@
 # Copyright 2020 Matthew Egan Odendahl
 # SPDX-License-Identifier: Apache-2.0
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this module except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # TODO: fill in docstrings for all modules
 # TODO: Organize into private modules for utility and public modules for interface?
 #  Maybe split Lissp from Hissp?
