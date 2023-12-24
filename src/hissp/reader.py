4d5472cd2f12fc6dc081eb6bbcf396f192aa2225
gilch <gilch@users.noreply.github.com>
2023-12-24 14:12:53 -0700

    Add raw symbols
    Common Lisp–style escapes.

--- src/hissp/reader.py
+++ src/hissp/reader.py
@@ -85,8 +85,16 @@ TOKENS = re.compile(
         )*  # Zero or more times.
       "  # Close quote.
      )
+    |(?P<injection>
+      [|]  # open
+        (?:[^|\\]  # Any non-magic character.
+           |\\(?:.|\n)  # Backslash only if paired, including with newline.
+        )*
+      [|]  # close
+     )
     |(?P<continue>
        [#]?"  # String not closed.
+      |[|]  # Injection not closed.
       |;.*  # Comment may need another line.
      )
     |(?P<atom>(?:[^\\ \n"();]|\\.)+)  # Let Python deal with it.
@@ -259,6 +267,7 @@ class Lissp:
             elif k == "close":    return self._close()
             elif k == "macro":    yield from self._macro(v)
             elif k == "string":   yield self._string(v)
+            elif k == "injection":yield self._injection(v)
             elif k == "continue": raise self._continue()
             elif k == "atom":     yield self.atom(v)
             else:                 raise self._error(k)
@@ -497,6 +506,9 @@ class Lissp:
         else:  # raw
             val = v[1:-1]  # Only remove quotes.
         return v if (v := pformat(val)).startswith("(") else f"({v})"
+
+    def _injection(self, v):
+        return re.sub(r"(?s)\\(.)", R'\1', v[1:-1])
 
     def _continue(self):
         return SoftSyntaxError("Incomplete string token.", self.position())
